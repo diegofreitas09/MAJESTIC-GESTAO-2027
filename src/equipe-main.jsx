f37772112fc58582ebe289889fe0af4e47cbaba8
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-29 11:54:36 -0300

    feat: adiciona bootstrap do app da equipe

---
+++ src/equipe-main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EquipeApp from './EquipeApp.jsx';
+import './styles.css';
+import './gestao-sucesso-catalogo.css';
+import './equipe-app.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EquipeApp />
+  </React.StrictMode>,
+);
