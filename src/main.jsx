008899607054d76c298116089bf4131c41c8af08
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-29 12:16:41 -0300

    feat: protege app executivo com login da direcao

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import AuthGate from './AuthGate.jsx';
 import './styles.css';
 import './gestao-sucesso-catalogo.css';
 import './auth-notifications.css';
 import './crm-executivo.css';
+import './auth.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthGate appName="Majestic Executivo 2027" allowedRoles={['direcao']}>
+      {()=> <App/>}
+    </AuthGate>
   </React.StrictMode>,
 );
