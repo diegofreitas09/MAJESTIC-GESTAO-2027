a10f3d21825d34123c16909f35f9f7c1c50806d1
diegofreitas09 <diegofreitas090707@gmail.com>
2026-08-29 12:16:52 -0300

    feat: protege app da equipe com login sincronizado

--- src/equipe-main.jsx
+++ src/equipe-main.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import EquipeApp from './EquipeApp.jsx';
+import AuthGate from './AuthGate.jsx';
 import './styles.css';
 import './gestao-sucesso-catalogo.css';
 import './equipe-app.css';
 import './atendimento-crm.css';
+import './auth.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <EquipeApp />
+    <AuthGate appName="Majestic Atendimento 2027" allowedRoles={['gestao','matricula','direcao']}>
+      {({profile,logout})=> <EquipeApp profile={profile} logout={logout}/>} 
+    </AuthGate>
   </React.StrictMode>,
 );
